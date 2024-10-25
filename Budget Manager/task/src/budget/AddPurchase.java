283ce1c3e54ab12d20089de4ecc0e8fa5e4dce84
ranudero <landerverbruggh@gmail.com>
2024-10-25 14:26:07 +0200

    feat: modify the Analyzer class to AddPurchase and make use of the Purchase class for adding.

---
+++ Budget Manager/task/src/budget/AddPurchase.java
@@ -0,0 +1,24 @@
+package budget;
+
+import java.util.Scanner;
+
+public class AddPurchase {
+
+    private final TransactionManager manager;
+
+    public AddPurchase(TransactionManager manager) {
+        this.manager = manager;
+    }
+
+    public void purchaseInput(){
+
+        String purchaseName = KeyboardUtil.getInputString("\nEnter purchase name:");
+        double purchasePrice = KeyboardUtil.getInputDouble("Enter its price:");
+
+        Purchase purchase = new Purchase(purchaseName, purchasePrice);
+
+        //TODO ADD PURCHASE TO THE LIST
+        manager.registerPurchase(purchase);
+
+    }
+}
