7d5677bd8857adf0fa9372bcb57fd22e8693d452
ranudero <landerverbruggh@gmail.com>
2024-10-28 13:46:56 +0100

    feat: add the PurchaseType Enum with different types of Purchases

---
+++ Budget Manager/task/src/budget/PurchaseType.java
@@ -0,0 +1,18 @@
+package budget;
+
+public enum PurchaseType {
+    FOOD("Food"),
+    CLOTHES("Clothes"),
+    ENTERTAINMENT("Entertainment"),
+    OTHER("Other");
+
+    private final String name;
+
+    PurchaseType(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
