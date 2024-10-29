914138542f1ea72e5fe698f8f475490d0ce61891
ranudero <landerverbruggh@gmail.com>
2024-10-29 13:58:10 +0100

    feat : add save and load to the main menu

--- Budget Manager/task/src/budget/menu/MainMenu.java
+++ Budget Manager/task/src/budget/menu/MainMenu.java
@@ -23,6 +23,8 @@ public class MainMenu {
         System.out.println("2) Add purchase");
         System.out.println("3) Show list of purchases");
         System.out.println("4) Balance");
+        System.out.println("5) Save");
+        System.out.println("6) Load");
         System.out.println("0) Exit");
         handleUserInput();
     }
